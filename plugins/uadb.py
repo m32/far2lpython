e55c4f315d6829976a21c27385d98d7d8ce60fbc
m32 <grzegorz.makarewicz@gmail.com>
2023-04-26 02:16:12 +0200

    dlugo ale poprawniej

--- plugins/uadb.py
+++ plugins/uadb.py
@@ -1,6 +1,9 @@
 import os
 import stat
+import re
 import logging
+from datetime import datetime
+
 from far2l.plugin import PluginVFS
 from far2l.fardialogbuilder import (
     Spacer,
@@ -12,8 +15,6 @@ from far2l.fardialogbuilder import (
     DialogBuilder
 )
 
-import stat
-import datetime
 from adbutils import AdbClient, AdbError
 
 log = logging.getLogger(__name__)
@@ -38,6 +39,217 @@ FILE_ATTRIBUTE_NO_SCRUB_DATA        =0x00020000
 FILE_ATTRIBUTE_BROKEN               =0x00200000
 FILE_ATTRIBUTE_EXECUTABLE           =0x00400000
 
+TICKSPERSEC        =10000000
+TICKSPERMSEC       =10000
+SECSPERDAY         =86400
+SECSPERHOUR        =3600
+SECSPERMIN         =60
+MINSPERHOUR        =60
+HOURSPERDAY        =24
+EPOCHWEEKDAY       =1  #/* Jan 1, 1601 was Monday */
+DAYSPERWEEK        =7
+MONSPERYEAR        =12
+
+
+class Entry:
+    def __init__(self, perms=None, links=1, uid=0, gid=0, size=None,
+                 date_time=None, date=None, name=None):
+        """initialize file"""
+        self.mode = 0
+        self.perms = perms
+        self.links = links
+        self.uid = uid
+        self.gid = gid
+        self.size = int(size) if size else 0
+        self.date_time = date_time
+        self.name = name
+        self.date = date
+
+        self.dirname = ''
+        self.link_target = None
+        self.filepath = None
+
+    def update(self, dirname):
+        """update object fields"""
+        month_num = {'Jan': 1,
+                     'Feb': 2,
+                     'Mar': 3,
+                     'Apr': 4,
+                     'May': 5,
+                     'Jun': 6,
+                     'Jul': 7,
+                     'Aug': 8,
+                     'Sep': 9,
+                     'Oct': 10,
+                     'Nov': 11,
+                     'Dec': 12}
+        self.dirname = dirname
+        if self.date_time:
+            date = self.date_time.split()
+            date = '%s-%02d-%s %s' % (date[1],
+                                      month_num[date[0]],
+                                      date[3],
+                                      date[2])
+            date = datetime.strptime(date, '%d-%m-%Y %H:%M:%S')
+        else:
+            date = datetime.strptime(self.date, '%Y-%m-%d %H:%M')
+
+        self.date = date
+        self.date_time = date.strftime('%Y-%m-%d %H:%M')
+
+        type = self.perms[0] if self.perms else None
+
+        if type == 'l' and ' -> ' in self.name:
+            self._correct_link()
+
+        self.filepath = os.path.join(self.dirname, self.name)
+        self.perms2mode()
+
+    def perms2mode(self):
+        perms = self.perms[1:]
+        lperms = len(perms)
+        mode = 0
+        # Check if 'perms' has the right format
+        if (
+            not [x for x in perms if x not in '-rwxXsStT']
+            and lperms == 9
+        ) :
+            pos = lperms - 1
+            for c in perms:
+                if c in 'sStT' :
+                    # Special modes
+                    mode += (1 << pos // 3) << 9
+
+                mode += 1 << pos if c in 'rwxst' else 0
+                pos -= 1
+        self.mode = mode | {
+            '-': 0,
+            'b': stat.S_IFBLK,
+            'c': stat.S_IFCHR,
+            'd': stat.S_IFDIR,
+            'l': stat.S_IFLNK,
+            'p': 0, #stat.S_IF???,
+            's': 0, #stat.S_IF???,
+        }[self.perms[0]]
+
+    def _correct_link(self):
+        """Canonize filename and fill the link attr"""
+        try:
+            name, target = self.name.split(' -> ')
+        except ValueError:
+            return
+
+        self.name = name
+
+        if not self.size:
+            self.size = 0
+
+        if target.startswith('/'):
+            self.link_target = target
+        else:
+            self.link_target = os.path.abspath(os.path.join(self.dirname, target))
+
+    def mk_link_relative(self):
+        """Convert links to relative"""
+        self.link_target = os.path.relpath(self.link_target, self.dirname)
+
+    def __str__(self):
+        if not self.name:
+            return ''
+
+        template = ('{mode:4o} {perms} {links:>4} {uid:<8} {gid:<8} {size:>8} {date} {name}')
+        return template.format(**self.__dict__)
+
+class Shell:
+    boxes = {
+        'busybox': {
+            'ls': 'busybox ls -anel {}',
+            'rls': 'busybox ls -Ranel {}',
+            'file_re': r'^(?P<perms>[-bcdlps][-rwxsStT]{9})\s+'
+                        r'(?P<links>\d+)\s'
+                        r'(?P<uid>\d+)\s+'
+                        r'(?P<gid>\d+)\s+'
+                        r'(?P<size>\d+)\s[A-Z,a-z]{3}\s'
+                        r'(?P<date_time>[A-Z,a-z]{3}\s+'
+                        r'\d+\s\d{2}:\d{2}:\d{2}\s+\d{4})\s'
+                        r'(?P<name>.*)'
+        },
+        'toolbox': {
+            'ls': 'ls -anl {}',
+            'rls': 'ls -Ranl {}',
+            'file_re': r'^(?P<perms>[-bcdlps][-rwxsStT]{9})\s+'
+                        r'(?P<links>\d+)\s'
+                        r'(?P<uid>\d+)\s+'
+                        r'(?P<gid>\d+)\s+'
+                        r'(?P<size>\d+)?\s'
+                        r'(?P<date>\d{4}-\d{2}-\d{2}\s'
+                        r'\d{2}:\d{2})\s'
+                        r'(?P<name>.*)'
+        },
+        'toybox': {
+            'ls': 'toybox ls -anl {}',
+            'rls':  'toybox ls -Ranl {}',
+            'file_re': r'^(?P<perms>[-bcdlps][-rwxsStT]{9})\s+'
+                        r'(?P<links>\d+)\s+'
+                        r'(?P<uid>\d+)\s+'
+                        r'(?P<gid>\d+)\s+'
+                        r'(?P<size>\d+)?\s'
+                        r'(?P<date>\d{4}-\d{2}-\d{2}\s'
+                        r'\d{2}:\d{2})\s'
+                        r'(?P<name>.*)'
+        }
+    }
+    def __init__(self, dev):
+        box = None
+        for name in ('busybox', 'toolbox', 'toybox'):
+            line = dev.shell('which '+name).strip()
+            if line:
+                box = name
+                break
+        else:
+            raise IOError(2, 'unknown shell')
+        self.box = self.boxes[box]
+        self.file_re = re.compile(self.box['file_re'])
+
+    def list(self, dev, top):
+        dtop = top
+        if dtop[-1] != '/':
+            dtop += '/'
+        cmd = self.box['ls'].format(dtop)
+        lines = dev.shell(cmd)
+        result = []
+        for line in lines.split('\n'):
+            line = line.strip()
+            if not line:
+                continue
+            rm = self.file_re.match(line)
+            if not rm:
+                continue
+            entry = Entry(**rm.groupdict())
+            entry.update(top)
+            if entry.perms[0] == 'l':
+                self.listfix(dev, entry, entry.link_target)
+            result.append(entry)
+        return result
+
+    def listfix(self, dev, entry, target):
+        lines = dev.shell('stat '+target).split('\n')
+        target = lines[0].split(' -> ')
+        if len(target) == 2:
+            target = target[1].strip()[1:-1]
+            return self.listfix(dev, entry, target)
+        try:
+            perms = lines[3].split(')')[0]
+            perms = perms.split('(')[1]
+            perms = perms.split('/')[1]
+            if perms[0] != 'd':
+                entry.size = int(lines[1].split()[1])
+            entry.perms = perms
+            entry.perms2mode()
+        except IndexError:
+            # link to non existing element
+            pass
+
 class Plugin(PluginVFS):
     label = "Python ADB"
     openFrom = ["PLUGINSMENU", "DISKMENU"]
@@ -55,13 +267,14 @@ class Plugin(PluginVFS):
         for d in self.clt.list():
             self.addName(d.serial, FILE_ATTRIBUTE_DIRECTORY, 0)
 
+
     def setName(self, i, name, attr, size):
         self.names[i] = self.s2f(name)
         item = self.Items[i].FindData
         item.dwFileAttributes = attr
-        item.nPhysicalSize = size
         item.nFileSize = size
         item.lpwszFileName = self.names[i]
+        return item
 
     def addName(self, name, attr, size):
         #log.debug('addName({})'.format(name))
@@ -76,22 +289,48 @@ class Plugin(PluginVFS):
     def addResult(self, result):
         n = 0
         for rec in result:
-            if rec.path in ('.', '..'):
+            if rec.name in ('.', '..'):
                 continue
             n += 1
         self.Items = self.ffi.new("struct PluginPanelItem []", n)
         self.names = [None]*n
         i = 0
         for rec in result:
-            if rec.path in ('.', '..'):
+            if rec.name in ('.', '..'):
                 continue
-            mode = stat.filemode(rec.mode)
-            if mode[0] == 'd':
-                attr = FILE_ATTRIBUTE_DIRECTORY
-            else:
-                attr = FILE_ATTRIBUTE_ARCHIVE
+            attr = 0
+            attr |= FILE_ATTRIBUTE_DIRECTORY if rec.mode&stat.S_IFDIR else 0
+            attr |= FILE_ATTRIBUTE_DEVICE if rec.mode&stat.S_IFCHR else 0
+            attr |= FILE_ATTRIBUTE_ARCHIVE if rec.mode&stat.S_IFREG else 0
+            #log.debug('{} mode={:5o} attr={} perms={}'.format(rec.name, rec.mode, attr, rec.perms))
             #datetime.datetime.fromtimestamp(rec.time).strftime('%Y-%m-%d %H:%M:%S')
-            self.setName(i, rec.path, attr, rec.size)
+            item = self.setName(i, rec.name, attr, rec.size)
+            item.dwUnixMode = rec.mode
+            # local timezone is in use
+            t = rec.date
+            if t.month < 3:
+                month = t.month + 13
+                year = t.year -1
+            else:
+                month = t.month + 1
+                year = t.year
+            cleaps = (3 * (year // 100) + 3) // 4
+            day = (
+                (36525 * year) // 100 - cleaps +
+                (1959 * month) // 64 +
+                t.weekday() -
+                584817
+            )
+
+            t = ((((
+                day * HOURSPERDAY
+                + t.hour) * MINSPERHOUR + 
+                t.minute) * SECSPERMIN +
+                t.second ) * 1000 +
+                0
+            ) * TICKSPERMSEC
+            item.ftLastWriteTime.dwHighDateTime = t >> 32
+            item.ftLastWriteTime.dwLowDateTime = t & 0xffffffff
             i += 1
 
     def deleteNames(self, names):
@@ -227,7 +466,8 @@ class Plugin(PluginVFS):
                 return False
         else:
             try:
-                result = self.device.sync.list(self.devicepath)
+                shell = Shell(self.device)
+                result = shell.list(self.device, self.devicepath)
                 self.addResult(result)
             except AdbError as ex:
                 log.exception('AdbError')
